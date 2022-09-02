f96b97b10662f07d257af75ab3bab3b2e98ec37e
seanf316 <seanfinn316@gmail.com>
2022-09-02 23:55:44 +0000

    BEM Naming structure added to Header and Footer sections

--- vanguard.js
+++ vanguard.js
@@ -1,5 +1,5 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".header__hamburger");
+const navMenu = document.querySelector(".header__navbar__nav-menu");
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -8,34 +8,13 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 
-const navLink = document.querySelectorAll(".nav-link");
+const navLink = document.querySelectorAll(".header__navbar__nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}
-//Get the button:
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-    scrollFunction()
-};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
-    }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
